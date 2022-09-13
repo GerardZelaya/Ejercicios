b618efbf342db68a64b8797e63ea7b6d6b05891a
GerardZelaya <113397213+GerardZelaya@users.noreply.github.com>
2022-09-12 18:09:49 -0600

    Add files via upload

---
+++ Ejercicios 2D #31/Unidad3Semana6Clase18Ejercicio2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> nums;
+	nums.push_back(24);
+	nums.push_back(81);
+	nums.push_back(57);
+	
+	for(int num: nums)
+	   cout << num << " ";
+}
